04a853a58fcabd5996ba06c3e8119e6bb9a96fdd
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-08-09 19:30:44 +0530

    Make Code Executablegit add .

--- main.go
+++ main.go
@@ -1,80 +1,72 @@
 package main // This programs is built into an executable code.
 
 import (
+	"flag"
 	"fmt"
 
-	"github.com/kasodeep/complete-go/arrays"
-	"github.com/kasodeep/complete-go/conditionals"
-	"github.com/kasodeep/complete-go/errors"
-	"github.com/kasodeep/complete-go/functions"
-	"github.com/kasodeep/complete-go/interfaces"
-	"github.com/kasodeep/complete-go/loops"
-	"github.com/kasodeep/complete-go/maps"
-	"github.com/kasodeep/complete-go/pointers"
-	"github.com/kasodeep/complete-go/structs"
 	"github.com/kasodeep/complete-go/variables"
 )
 
+// This function is the start of the program.
 func main() {
 	fmt.Println("----- Starting Go Demo -----")
-	fmt.Println("Hello, Go!")
 
-	fmt.Println()
-	fmt.Println("----- Variables Demo -----")
-	variables.Types()
-	variables.ShortNotation()
-	variables.TypeConversion()
-	variables.Constants()
-	variables.LogPrinting()
-	variables.RuneDemo()
+	// specify the package.
+	packageName := flag.String("package", "variables", "This flag calls the specified package.")
+	flag.Parse()
 
-	fmt.Println()
-	fmt.Println("----- Conditionals Demo -----")
-	conditionals.IfDemo()
-	conditionals.SwitchDemo()
-
-	fmt.Println()
-	fmt.Println("----- Functions Demo -----")
-	functions.FunctionsDemo()
-	functions.TestReports()
-
-	fmt.Println()
-	fmt.Println("----- Structs Demo -----")
-	structs.StructsDemo()
-	structs.Anonymous()
-	structs.EmbeddedDemo()
-	structs.StructMethods()
-	structs.SizeDemo()
-	structs.EmptyStruct()
-
-	fmt.Println()
-	fmt.Println("----- Interfaces Demo -----")
-	interfaces.InterfaceDemo()
-	interfaces.StructInterfaceTypes()
-
-	fmt.Println()
-	fmt.Println("----- Errors Demo -----")
-	errors.ErrorDemo()
-	errors.CustomError()
-	errors.ErrorPackage()
-
-	fmt.Println()
-	fmt.Println("----- Loops Demo -----")
-	loops.ForDemo()
-
-	fmt.Println()
-	fmt.Println("----- Arrays Demo -----")
-	arrays.ArrayDemo()
-	arrays.VariadicDemo()
-	arrays.MatrixDemo()
-	arrays.RangeDemo()
-
-	fmt.Println()
-	fmt.Println("----- Maps Demo -----")
-	maps.MapsDemo()
-
-	fmt.Println()
-	fmt.Println("----- Pointers Demo -----")
-	pointers.PointersDemo()
-	pointers.ReceiverDemo()
+	switch *packageName {
+	case "variables":
+		variables.Index()
+	}
 }
+
+// fmt.Println()
+// 	fmt.Println("----- Conditionals Demo -----")
+// 	conditionals.IfDemo()
+// 	conditionals.SwitchDemo()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Functions Demo -----")
+// 	functions.FunctionsDemo()
+// 	functions.TestReports()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Structs Demo -----")
+// 	structs.StructsDemo()
+// 	structs.Anonymous()
+// 	structs.EmbeddedDemo()
+// 	structs.StructMethods()
+// 	structs.SizeDemo()
+// 	structs.EmptyStruct()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Interfaces Demo -----")
+// 	interfaces.InterfaceDemo()
+// 	interfaces.StructInterfaceTypes()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Errors Demo -----")
+// 	errors.ErrorDemo()
+// 	errors.CustomError()
+// 	errors.ErrorPackage()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Loops Demo -----")
+// 	loops.ForDemo()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Arrays Demo -----")
+// 	arrays.ArrayDemo()
+// 	arrays.VariadicDemo()
+// 	arrays.MatrixDemo()
+// 	arrays.RangeDemo()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Maps Demo -----")
+// 	maps.MapsDemo()
+
+// 	fmt.Println()
+// 	fmt.Println("----- Pointers Demo -----")
+// 	pointers.PointersDemo()
+// 	pointers.ReceiverDemo()
---
+++ variables/index.go
@@ -0,0 +1,19 @@
+package variables
+
+import "fmt"
+
+func Index() {
+	fmt.Println("----- Variables Demo -----")
+
+	// displaying the types.
+	Types()
+
+	// notation and type conversion.
+	ShortNotation()
+	TypeConversion()
+
+	// extra knowledge.
+	Constants()
+	LogPrinting()
+	RuneDemo()
+}
